76f118ceb695cca2d2d10599fa4dcb8c54dee77b
Ali Maher <ali.abdelhafez0@gmail.com>
2023-12-07 21:15:57 +0200

    feat: init repo

---
+++ apps/express/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'express',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': '@swc/jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/express',
+};
